a71772eeceeb84391b98ee8135e2ce24f97c6120
Mason <116673955+king-mason@users.noreply.github.com>
2023-11-08 09:58:43 -0800

    DFS and method improvements

--- Algorithm.py
+++ Algorithm.py
@@ -1,24 +1,25 @@
 from collections import deque
+from math import sqrt
 
 def BFS_basic(G, snake):
     snake_head = snake[0]
 
-    Q = deque([[snake_head]])
-    dist = {snake_head: 0}
+    Q = deque([snake_head])
+    depth = {snake_head: 0}
     E = set()
     snake_set = set(snake)
 
     while Q:
-        path = Q.popleft()
-        v = path[-1]
-        if len(path) <= len(snake):
-            if snake[-len(path)] in E:
-                E.remove(snake[-len(path)])
-        for num in G[v]:
-            if num not in E.union(snake_set):
-                Q.append(path + [num])
-                E.add(num)
-                dist[num] = dist[v] + 1
+        v = Q.popleft()
+        if depth[v]+1 <= len(snake):
+            if snake[-(depth[v]+1)] in E:
+                E.remove(snake[-(depth[v]+1)])
+        for node in G[v]:
+            if node not in E:
+                E.add(node)
+                depth[node] = depth[v] + 1
+                if node not in snake_set:
+                    Q.append(node)
     return E
 
 def BFS(G, snake, apple):
@@ -29,36 +30,80 @@ def BFS(G, snake, apple):
 
     resort = []
     Q = deque([[snake_head]])
-    dist = {snake_head: 0}
-    E = set()
-    for node in snake:
-        E.add(node)
+    E = set(snake)
 
     while Q:
         path = Q.popleft()
         v = path[-1]
+
         if len(path) <= len(snake):
             if snake[-len(path)] in E:
                 E.remove(snake[-len(path)])
-        for num in G[v]:
-            if num not in E:
-                Q.append(path + [num])
-                E.add(num)
-                dist[num] = dist[v] + 1
-                if num == apple:
-                    if len(path) == 1:
-                        return apple
+        
+        for node in G[v]:
+            if node not in E:
+                Q.append(path + [node])
+                E.add(node)
+                if node == apple:
+                    score = len(BFS_basic(G, (path[1:] + [node]))) / (len(G))
+                    print(score)
+                    if score > 0.8:
+                        return (path[1:] + [node])[::-1]
                     else:
-                        score = len(BFS_basic(G, (path[1:] + [num]))) / (len(G))
-                        print(score)
-                        if score > 0.8:
-                            return (path[1:] + [num])[::-1]
-                        else:
-                            resort.append((score, (path[1:] + [num])[::-1]))
+                        resort.append((score, (path[1:] + [node])[::-1]))
 
-    resort.sort(reverse=True)
-    print(resort)
-    return resort[0]
+    # resort.sort(reverse=True)
+    # print(resort)
+    # return resort[0]
+    return []
+
+
+def DFS_long_path(G, snake, apple):
+    snake_head = snake[0]
+
+    Q = [[snake_head]]
+    depth = {snake_head: 0}
+    E = set(snake)
+    longest_path = []
+    
+    accessible_nodes = BFS_basic(G, snake)
+    for i in range(len(snake)):
+        body = snake[len(snake) - (i+1)]
+        if body in accessible_nodes:
+            target = body
+            dist_from_tail = i
+            break
+
+    while Q:
+        path = Q.pop()
+        v = path[-1]
+        E.add(v)
+        adj_nodes = G[v]
+        adj_nodes.sort(key = lambda x: absolute_distance(x, target))
+        for node in adj_nodes:
+            new_path = path + [node]
+            if node not in E:
+                # E.add(node)
+                Q.append(new_path)
+                depth[node] = depth[v] + 1
+                # Find out if at any point the snake could reach the target point
+                # as the tail leaves that point
+                if travel_distance(node, target) == dist_from_tail - depth[node]:
+                    new_snake = new_path[:0:-1] + [snake[i] for i in range(len(snake)-depth[node])]
+                    return BFS(G, new_snake, apple) + new_path[:0:-1]
+                print(new_path)
+                longest_path = max(longest_path, new_path, key=len)
+            else:
+                print(node)
+    
+    return longest_path[:0:-1]
+
+
+def absolute_distance(a, b):
+    return sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+def travel_distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 def create_adjacent_grid(x, y):
     grid = {}
@@ -83,4 +128,13 @@ grid = create_adjacent_grid(x, y)
 
 
 #print(grid)
-(BFS(grid, [(1, 2), (1, 3), (1, 4)], (5, 2)))
+# print(BFS(grid, [(1, 2), (1, 3), (1, 4)], (5, 2)))
+snake = deque([(2, 2), (2, 3), (2, 4), (1, 4), (1, 5), (2, 5), (3, 5), (4, 5), 
+                   (4, 4), (4, 3), (4, 2), (4, 1), (4, 0), (3, 0), (2, 0), (1, 0),
+                     (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (1, 9)])
+print(BFS_basic(grid, snake))
+print(BFS(grid, snake, (9, 9)))
+print(DFS_long_path(grid, snake, (9, 9)))
+
+# [(1, 4), (1, 3), (1, 2), (1, 1), (2, 1), (3, 1), (3, 2)]
+
--- Main.py
+++ Main.py
@@ -1,19 +1,26 @@
 import pygame
 from collections import deque
 import random
-from Algorithm import create_adjacent_grid, BFS
+from Algorithm import create_adjacent_grid, BFS, DFS_long_path
 import sys
 
 x = 40
 y = 20
-blocksize = 30
+block_size = 30
+border_size = 30
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
 GREEN = "green"
 RED = "red"
-WINDOW_HEIGHT = y*blocksize
-WINDOW_WIDTH = x*blocksize
-snake = deque([(1, 1), (1, 2)])
+WINDOW_HEIGHT = y*block_size
+WINDOW_WIDTH = x*block_size
+# snake = deque([(1, 1), (2, 1), (2, 1), (2, 1), (2, 1), (2, 1), (2, 1), (2, 1), (2, 1), (2, 1), (2, 1)])
+snake = deque([(2, 2), (2, 3), (2, 4), (1, 4), (1, 5), (2, 5), (3, 5), (4, 5), 
+                   (4, 4), (4, 3), (4, 2), (4, 1), (4, 0), (3, 0), (2, 0), (1, 0),
+                     (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)])
+# snake = deque([(2, 2), (2, 3), (2, 4), (1, 4), (1, 5), (2, 5), (3, 5), (4, 5), 
+#                    (4, 4), (4, 3), (4, 2), (4, 1), (4, 0), (3, 0), (2, 0), (1, 0),
+#                      (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (1, 9)])
 
 
 def main():
@@ -29,25 +36,37 @@ def main():
     all_points = list(grid.keys())
     apple = random.choice(list(set(all_points)-set(snake)))
     last_apple = None
-    delay = 50
+    delay = 10
 
     current_path = BFS(grid, snake, apple)
+    if not current_path:
+        current_path = DFS_long_path(grid, snake, apple)
 
     while True:
         drawGrid()
+        drawBorder(*snake[0])
+        if not current_path:
+            pygame.display.update()
+            break
         next_space = current_path.pop()
+
 
         if next_space == apple:
             last_apple = apple
             snake.appendleft(apple)
             apple = random.choice(list(set(all_points)-set(snake)))
             current_path = BFS(grid, snake, apple)
+            if not current_path:
+                current_path = DFS_and_BFS(grid, snake, apple)
+                print(current_path)
+                print(apple)
+                print(last_apple)
 
         else:
             snake.appendleft(next_space)
 
         if next_space != last_apple:
-            #what happens if you collect another apple before the tail reaches
+            # what happens if you collect another apple before the tail reaches
             snake.pop()
 
         for event in pygame.event.get():
@@ -57,19 +76,66 @@ def main():
 
         pygame.display.update()
         pygame.time.wait(delay)
+    
+    # Game over
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 def drawGrid():
-    for x in range(0, WINDOW_WIDTH, blocksize):
-        for y in range(0, WINDOW_HEIGHT, blocksize):
-            rect = pygame.Rect(x, y, blocksize, blocksize)
+    # for i in range(len(snake)):
+        # if i != 0:
+        #     prev_body = snake[i-1]
+        # if i != len(snake):
+        #     next_body = snake[i+1]
+            # node = (i, j)
+            # neighbors = []
+
+            # # Add the neighboring nodes to the list (excluding diagonals)
+            # for dx, dy in [(0, -1), (-1, 0), (1, 0), (0, 1)]:
+            #     new_x, new_y = i + dx, j + dy
+            #     if 0 <= new_x < x and 0 <= new_y < y and (new_x, new_y) != node:
+            #         neighbors.append((new_x, new_y))
+
+    for x in range(0, WINDOW_WIDTH, block_size):
+        for y in range(0, WINDOW_HEIGHT, block_size):
+            
+            rect = pygame.Rect(x, y, block_size, block_size)
             pygame.draw.rect(SCREEN, BLACK, rect) #change to WHITE for grid
 
     for body_x, body_y in snake:
-        body_rect = pygame.Rect(body_x*blocksize, body_y*blocksize, blocksize, blocksize)
+        body_rect = pygame.Rect(body_x*block_size, body_y*block_size, block_size, block_size)
         pygame.draw.rect(SCREEN, GREEN, body_rect)
 
-    body_rect = pygame.Rect(apple[0]*blocksize, apple[1]*blocksize, blocksize, blocksize)
+    body_rect = pygame.Rect(apple[0]*block_size, apple[1]*block_size, block_size, block_size)
     pygame.draw.rect(SCREEN, RED, body_rect)
+
+def drawBorder(x, y, location='top', color='blue'):
+    if location == 'top':
+        length = block_size
+        height = border_size
+        a = 0
+        b = 0
+    elif location == 'bottom':
+        length = block_size
+        height = border_size
+        a = 0
+        b = block_size - height
+    elif location == 'left':
+        length = border_size
+        height = block_size
+        a = 0
+        b = 0
+    elif location == 'right':
+        length = border_size
+        height = block_size
+        a = block_size - height
+        b = 0
+
+    border_rect = pygame.Rect(x*block_size+a, y*block_size+b, length, height)
+    pygame.draw.rect(SCREEN, color, border_rect)
 
 main()
 
